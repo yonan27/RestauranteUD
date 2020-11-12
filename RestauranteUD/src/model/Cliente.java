300f08415fc3f98b2379ba5b9be5a01a32d2028b
bonillaaitor <62545298+bonillaaitor@users.noreply.github.com>
2020-11-12 21:54:09 +0100

    Cliente y persona
    Contructores, get and set

--- RestauranteUD/src/model/Cliente.java
+++ RestauranteUD/src/model/Cliente.java
@@ -4,6 +4,42 @@ public class Cliente extends Persona {
 	
 	private long numTarjeta;
 	private Suscripcion suscripcion;
+	
+	public Cliente(long numTarjeta, Suscripcion suscripcion) {
+		super();
+		this.numTarjeta = numTarjeta;
+		this.suscripcion = suscripcion;
+	}
+	
+	public Cliente() {
+		super();
+		this.numTarjeta = 0;
+		this.suscripcion = null;
+	}
+
+	public long getNumTarjeta() {
+		return numTarjeta;
+	}
+
+	public void setNumTarjeta(long numTarjeta) {
+		this.numTarjeta = numTarjeta;
+	}
+
+	public Suscripcion getSuscripcion() {
+		return suscripcion;
+	}
+
+	public void setSuscripcion(Suscripcion suscripcion) {
+		this.suscripcion = suscripcion;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [numTarjeta=" + numTarjeta + ", suscripcion=" + suscripcion + "]";
+	}
+	
+	
+	
 
 
 }
--- RestauranteUD/src/model/Persona.java
+++ RestauranteUD/src/model/Persona.java
@@ -13,5 +13,103 @@ public class Persona {
 	protected Date fechaNacimiento;
 	private String direccion;
 	public static SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Persona(String usuario, String contra, String email, String nombre, String apellidos, Date fechaNacimiento,
+			String direccion) {
+		super();
+		this.usuario = usuario;
+		this.contra = contra;
+		this.email = email;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+		this.direccion = direccion;
+	}
+	
+	public Persona() {
+		super();
+		this.usuario = "";
+		this.contra = "";
+		this.email = "";
+		this.nombre = "";
+		this.apellidos = "";
+		this.fechaNacimiento = null;
+		this.direccion = "";
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContra() {
+		return contra;
+	}
+
+	public void setContra(String contra) {
+		this.contra = contra;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public static SimpleDateFormat getDf() {
+		return df;
+	}
+
+	public static void setDf(SimpleDateFormat df) {
+		Persona.df = df;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [usuario=" + usuario + ", contra=" + contra + ", email=" + email + ", nombre=" + nombre
+				+ ", apellidos=" + apellidos + ", fechaNacimiento=" + fechaNacimiento + ", direccion=" + direccion
+				+ "]";
+	}
+	
+	
+	
+	
 
 }
