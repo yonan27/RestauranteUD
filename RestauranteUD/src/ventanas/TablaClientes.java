bb54c8e4dd0ccc283454f5704b51188d59f7ce01
grupob-07 <72814002+grupob-07@users.noreply.github.com>
2021-01-07 18:13:15 +0100

    tablacliente

--- RestauranteUD/src/ventanas/TablaClientes.java
+++ RestauranteUD/src/ventanas/TablaClientes.java
@@ -1,11 +1,36 @@
 package ventanas;
 
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 import model.Trabajador;
 
-public class TablaClientes {
+public class TablaClientes extends JFrame {
 
-	public static void abrirTablaClientes(Trabajador t) {
-		// TODO 
+	private JScrollPane tablaPanel;
+	private JPanel botonPanel;
+	private JButton anadirBoton;
+	private JButton eliminarBoton;
+	private JButton atrasBoton;
+	private JTable tabla;
+	private DefaultTableModel modelo;
+	
+	public TablaClientes(Trabajador t) {
+		this.setTitle ("tabla de cliente");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setData();
+		tablaPanel = new JScrollPane(tabla);
+		botonPanel.setLayout(new GridBagLayout());
+		anadirBoton = new JButton ("anadir cliente");
+		botonPanel.add(anadirBoton);
+		eliminarBoton = new JButton ("eleminar cliente");
 		
 	}
 
